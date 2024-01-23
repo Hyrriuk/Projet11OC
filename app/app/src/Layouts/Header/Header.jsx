00c2dd942e2f459772f717cef5da1e567f27edaa
Eddie <eddie-tayeb@hotmail.fr>
2024-01-23 13:46:28 +0100

    Modification de la logique d'affichage et des cookies lors de l'actualisation de la page

--- app/app/src/Layouts/Header/Header.jsx
+++ app/app/src/Layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from '../../assets/img/argentBankLogo.webp';
 import {Link} from 'react-router-dom';
 import {setLogout} from '../../Redux/Reducers/Signinreducer';
@@ -8,14 +8,23 @@ import './Header.css';
 
 function Header() {
     const token = useSelector((state) => state.signIn.token);
-
     const dataUser = useSelector((state) => state.profile);
     const dispatch = useDispatch();
+    const storedToken = Cookies.get('accessToken');
+
+    useEffect(() => {
+        // Vérifiez la présence du token dans les cookies et déconnectez l'utilisateur si le token est manquant
+        const storedToken = Cookies.get('accessToken');
+        if (!storedToken && token) {
+            dispatch(setLogout());
+        }
+    }, [dispatch, token]);
+
     const handleLogout = () => {
-        // si l'utilisateur est connecté, suppression du token pour se deconnecter
-        if (token) {
+        // si l'utilisateur est connecté, suppression du token pour se déconnecter
+        if (token || storedToken) {
             Cookies.remove('accessToken');
-            dispatch(setLogout({}));
+            dispatch(setLogout());
         }
     };
 
@@ -26,16 +35,23 @@ function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                {token && (
-                    <Link className="main-nav-item" to="/Connected">
+                {token || storedToken ? (
+                    <>
+                        <Link className="main-nav-item" to="/Connected">
+                            <i className="fa fa-user-circle"></i>
+                            {dataUser.userName}
+                        </Link>
+                        <Link className="main-nav-item" to="/" onClick={handleLogout}>
+                            <i className="fa fa-arrow-right fa-lg"></i>
+                            Sign Out
+                        </Link>
+                    </>
+                ) : (
+                    <Link className="main-nav-item" to="./Connection">
                         <i className="fa fa-user-circle"></i>
-                        {dataUser.userName}
+                        Sign-in
                     </Link>
                 )}
-                <Link className="main-nav-item" to={token ? '/' : './Connection'} onClick={handleLogout}>
-                    {token ? <i className="fa fa-arrow-right fa-lg"></i> : <i className="fa fa-user-circle"></i>}
-                    {token ? 'Sign Out' : 'Sign-in'}
-                </Link>
             </div>
         </nav>
     );
--- app/app/src/Pages/Connected/Connected.jsx
+++ app/app/src/Pages/Connected/Connected.jsx
@@ -1,34 +1,45 @@
-import React from "react";
-import Account from "../../Components/Account/Account";
-import EditName from "../../Components/EditName/EditName";
-import {Apicalls} from "../../serverapi/serverapi";
-import {setGetProfile} from "../../Redux/Reducers/ProfileUserReducer";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-import "./Connected.css";
+import React from 'react';
+import Account from '../../Components/Account/Account';
+import EditName from '../../Components/EditName/EditName';
+import {Apicalls} from '../../serverapi/serverapi';
+import {setGetProfile} from '../../Redux/Reducers/ProfileUserReducer';
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect, useState} from 'react';
+import Cookies from 'js-cookie';
+import './Connected.css';
 
 function Connected() {
     const token = useSelector((state) => state.signIn.token);
     const dataUser = useSelector((state) => state.profile);
-
     const dispatch = useDispatch();
     const [toggleEditName, setToggleEditName] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            //recuperation de la data utilisateur
             try {
-                const data = await Apicalls("getProfile", token, {});
+                // Vérifiez la présence du token dans les cookies
+                const storedToken = Cookies.get('accessToken');
+
+                if (!storedToken) {
+                    // Si le token n'est pas présent, redirigez vers la page Connection
+                    window.location.href = '/Connection';
+                    return;
+                }
+
+                // Si le token est présent, effectuez l'appel API getProfile
+                const data = await Apicalls('getProfile', storedToken, {});
 
                 dispatch(setGetProfile({data}));
             } catch (error) {
-                console.log(error, "error");
-                // Gère les erreurs de l'appel API
+                console.log(error, 'error');
+                dispatch(setLogout());
+                // Redirigez vers la page Connection en cas d'erreur
+                window.location.href = '/Connection';
             }
         };
 
         fetchData();
-    }, []);
+    }, [dispatch, history]);
 
     const handleEditName = () => {
         setToggleEditName(!toggleEditName);
@@ -40,10 +51,10 @@ function Connected() {
                 <h1>
                     Welcome back
                     <br />
-                    {dataUser.firstName + " " + dataUser.lastName + " !"}
+                    {dataUser.firstName + ' ' + dataUser.lastName + ' !'}
                 </h1>
                 <button className="edit-button" onClick={handleEditName}>
-                    {!toggleEditName ? "Edit name" : "cancel"}
+                    {!toggleEditName ? 'Edit name' : 'cancel'}
                 </button>
 
                 {toggleEditName && <EditName onSubmit={handleEditName} />}
@@ -51,23 +62,23 @@ function Connected() {
             <h2 className="sr-only">Accounts</h2>
             <Account
                 props={{
-                    accountNumber: "Argent Bank Checking (x8349)",
-                    balance: "$2,082.79",
-                    status: "Available Balance",
+                    accountNumber: 'Argent Bank Checking (x8349)',
+                    balance: '$2,082.79',
+                    status: 'Available Balance',
                 }}
             />
             <Account
                 props={{
-                    accountNumber: "Argent Bank Savings (x6712)",
-                    balance: "$10,928.42",
-                    status: "Available Balance",
+                    accountNumber: 'Argent Bank Savings (x6712)',
+                    balance: '$10,928.42',
+                    status: 'Available Balance',
                 }}
             />
             <Account
                 props={{
-                    accountNumber: "Argent Bank Credit Card (x8349)",
-                    balance: "$184.30",
-                    status: "Current Balance",
+                    accountNumber: 'Argent Bank Credit Card (x8349)',
+                    balance: '$184.30',
+                    status: 'Current Balance',
                 }}
             />
         </main>
